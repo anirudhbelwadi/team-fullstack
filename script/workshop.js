041bde925822d5d5dc11dcefe8b1f6da14587b63
anirudhbelwadi <anirudh.belwadi@gmail.com>
2021-07-11 15:42:30 +0400

    workshop page developed

--- script/workshop.js
+++ script/workshop.js
@@ -1,6 +1,6 @@
 $(window).scroll(function () {
     var scroll_amount = $(window).scrollTop();
-    if (scroll_amount > 100) {
+    if (scroll_amount > 30) {
         $(".navbar").addClass('navbar_resize');
         $(".navbar").removeClass('navbar_noresize');
         $(".navbar img").addClass('navbar_logo_resize');
@@ -22,3 +22,19 @@ $(window).scroll(function () {
 $('.navbar-nav>li>a').on('click', function () {
     $('.navbar-collapse').collapse('hide');
 });
+
+$("#source").change(function () {
+    sourceChange($(this).val());
+});
+
+$("#other_source_div").hide();
+function sourceChange(stateValue) {
+    if (stateValue == "Others") {
+        $("#other_source_div").fadeIn();
+        $("#other_source").prop("disabled", false);
+    }
+    else {
+        $("#other_source_div").hide();
+        $("#other_source").prop("disabled", true);
+    }
+}
